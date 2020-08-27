6d832cc3c38ef9353fb63b82322bde2718d0aadb
Michael Hoisie <hoisie@google.com>
2020-08-26 22:30:57 -0700

    Update to mockito-inline 3.5.4 and re-enable MockitoMockJavaFrameworkTest

--- integration_tests/mockito-experimental/src/test/java/org/robolectric/integrationtests/mockito/experimental/MockitoMockJavaFrameworkTest.java
+++ integration_tests/mockito-experimental/src/test/java/org/robolectric/integrationtests/mockito/experimental/MockitoMockJavaFrameworkTest.java
@@ -19,7 +19,6 @@ public class MockitoMockJavaFrameworkTest {
    * @see <a href="https://github.com/robolectric/robolectric/issues/5522">Issue 5522</a>
    */
   @Test
-  @Ignore("Enable when Mockito 3.5.4 is released")
   public void file_getAbsolutePath_isMockable() throws Exception {
     File file = mock(File.class);
     doReturn("absolute/path").when(file).getAbsolutePath();
